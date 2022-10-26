0abe65818bf2dd635ef75d20db7b6b70ccb71536
LasseDall <lass794rrstud.kea.dk>
2022-10-26 12:30:58 +0200

    User updated, WishRepository ArrayList super

--- src/main/java/com/example/pelican/model/User.java
+++ src/main/java/com/example/pelican/model/User.java
@@ -1,4 +1,38 @@
 package com.example.pelican.model;
 
+import java.util.ArrayList;
+
 public class User {
+
+
+  private int userID;
+  private String email;
+  private String userName;
+  private String fName;
+  private String lName;
+  private String password;
+  private ArrayList<Integer> sharedWishlists;
+
+
+  public User(int userID, String email, String userName, String fName, String lName, String password, ArrayList<Integer> sharedWishlists) {
+    this.userID = userID;
+    this.email = email;
+    this.userName = userName;
+    this.fName = fName;
+    this.lName = lName;
+    this.password = password;
+    this.sharedWishlists = sharedWishlists;
+  }
+
+  public int getUserID() {
+    return userID;
+  }
+
+  public void setSharedWishlists(ArrayList<Integer> sharedWishlists) {
+    this.sharedWishlists = sharedWishlists;
+  }
+
+  public ArrayList<Integer> getSharedWishlists() {
+    return sharedWishlists;
+  }
 }
--- src/main/java/com/example/pelican/repository/WishRepository.java
+++ src/main/java/com/example/pelican/repository/WishRepository.java
@@ -1,7 +1,63 @@
 package com.example.pelican.repository;
 
+import com.example.pelican.model.User;
 import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 @Repository
 public class WishRepository {
+
+  private ArrayList<User> users;
+
+  public ArrayList<User> userList() {
+      ArrayList<User> userList = new ArrayList<>();
+
+      try {
+        Connection conn = DriverManager.getConnection(
+            "jdbc:mysql://pelican.mysql.database.azure.com:3306/Pelican",
+            "pelifar", "1234Fuckmekanikeren");
+        PreparedStatement psts = conn.prepareStatement("SELECT * from user");
+        ResultSet resultSet = psts.executeQuery();
+
+        while (resultSet.next()){
+          int userID = resultSet.getInt(1);
+          String email = resultSet.getString(2);
+          String userName = resultSet.getString(3);
+          String fName = resultSet.getString(4);
+          String lName = resultSet.getString(5);
+          String password = resultSet.getString(6);
+          //Add shareList!
+          ArrayList<Integer> sharedWishLists = new ArrayList<>();
+          userList.add(new User(userID, email, userName, fName, lName, password, sharedWishLists));
+        }
+      } catch (SQLException sqle) {
+        System.out.println("Connection to server failed");
+        sqle.printStackTrace();
+      }
+      return userList;
+  }
+
+  public WishRepository() {
+    users = userList();
+  }
+
+  public void addUserToShare(int activeUserID, int shareWithUserID) {
+    User shareWithUser = findUserByID(shareWithUserID);
+    ArrayList<Integer> newShareList = shareWithUser.getSharedWishlists();
+    newShareList.add(activeUserID);
+    shareWithUser.setSharedWishlists(newShareList);
+    }
+
+  public User findUserByID(int userID) {
+    for (User user : users) {
+      if (user.getUserID() == userID) {
+        return user;
+      }
+    }
+    return null;
+  }
 }
