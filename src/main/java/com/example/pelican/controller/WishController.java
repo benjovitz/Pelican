25662fe0a5dbde727787d2d56a0c5f5e1460589d
benjovitz <dbenjovitz@gmail.com>
2022-10-27 15:09:38 +0200

    login modul

--- src/main/java/com/example/pelican/controller/WishController.java
+++ src/main/java/com/example/pelican/controller/WishController.java
@@ -1,47 +1,79 @@
 package com.example.pelican.controller;
 
 import com.example.pelican.model.User;
+import com.example.pelican.repository.UserRepository;
 import com.example.pelican.repository.WishRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class WishController {
 
     WishRepository wishRepository;
+    UserRepository userRepository;
 
-    public WishController(WishRepository u) {
-        wishRepository = u;
+    public WishController(WishRepository w, UserRepository u) {
+        wishRepository = w;
+        userRepository = u;
     }
 
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("users",wishRepository.userList());
+        model.addAttribute("users", userRepository.userList());
         return "index";
+    }
+
+    @GetMapping("/index")
+    public String index(@RequestParam("username")String username,@RequestParam("password")String password,Model model) {
+        boolean b = userRepository.loginCheck(username, password);
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        System.out.println(username);
+        System.out.println(password);
+        if (b == true) {
+            return "index";
+        } else {
+            return "redirect:/login";
+        }
+    }
+
+    @PostMapping("/index")
+    public String postIndex(@RequestParam("username")String username,@RequestParam("password")String password, RedirectAttributes redirectAttributes){
+        redirectAttributes.addAttribute("username",username);
+        redirectAttributes.addAttribute("password",password);
+        return "redirect:/index";
     }
 
 
     @GetMapping("/createNew.html")
-    public String create(){
+    public String create() {
         return "createNew";
     }
 
     @PostMapping("/createNew")
-    public String create(@ModelAttribute User user){
-        wishRepository.createUser(user);
+    public String create(@ModelAttribute User user) {
+        userRepository.createUser(user);
         return "redirect:/";
     }
 
     @GetMapping("/delete/{userID}")
-    public  String deleteUser(@PathVariable("userID") int deleteID) {
-        wishRepository.deleteUserById(deleteID);
+    public String deleteUser(@PathVariable("userID") int deleteID) {
+        userRepository.deleteUserById(deleteID);
         return "redirect:/";
     }
 
-    @GetMapping("/login")
-    public String login(@RequestParam("userName")String username){
-        return "login";
+    @PostMapping("/login")
+    public String logincheck(@RequestParam("username") String username, @RequestParam("password") String password, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addAttribute("username",username);
+        redirectAttributes.addAttribute("password",password);
+        return "redirect://login";
+
     }
-}
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+        }
+    }
---
+++ src/main/java/com/example/pelican/repository/UserRepository.java
@@ -0,0 +1,155 @@
+package com.example.pelican.repository;
+
+import com.example.pelican.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+@Repository
+public class UserRepository {
+
+    private ArrayList<User> users;
+    JdbcTemplate template;
+    public UserRepository(){
+        users=userList();
+    }
+
+    public ArrayList<User> userList() {
+        ArrayList<User> userList = new ArrayList<>();
+
+        try {
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://pelican.mysql.database.azure.com:3306/Pelican",
+                    "pelifar", "1234Fuckmekanikeren");
+            PreparedStatement psts = conn.prepareStatement("SELECT * from user");
+            ResultSet resultSet = psts.executeQuery();
+
+            while (resultSet.next()){
+                int userID = resultSet.getInt(1);
+                String email = resultSet.getString(2);
+                String userName = resultSet.getString(3);
+                String fName = resultSet.getString(4);
+                String lName = resultSet.getString(5);
+                String password = resultSet.getString(6);
+                String queryCreate = "SELECT relatedUserID FROM relationtable WHERE userID=?";
+                PreparedStatement rpsts = conn.prepareStatement(queryCreate);
+                rpsts.setInt(1, userID);
+                ResultSet relationResultSet = rpsts.executeQuery();
+                ArrayList<Integer> sharedWishLists = new ArrayList<>();
+
+                while (relationResultSet.next()) {
+                    sharedWishLists.add(relationResultSet.getInt(1));
+                }
+                userList.add(new User(userID, email, userName, fName, lName, password, sharedWishLists));
+            }
+
+        } catch (SQLException sqle) {
+            System.out.println("Connection to server failed");
+            sqle.printStackTrace();
+        }
+        return userList;
+    }
+
+    public boolean loginCheck(String username,String password){
+        boolean b = true;
+        User u = findUserByUserName(username);
+        if(u.getPassword().equals(password)){
+            return b;
+        } else{
+            b=false;
+            return b;
+        }
+    }
+    public void createUser(User user) {
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://pelican.mysql.database.azure.com:3306/Pelican",
+                    "pelifar", "1234Fuckmekanikeren");
+            String sql = "INSERT INTO user VALUES(?,?,?,?,?,?)";
+
+            template.update(sql, user.getUserID(), user.getEmail(), user.getUserName(), user.getfName(), user.getlName(), user.getPassword());
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void deleteUserById(int deleteID) {
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://pelican.mysql.database.azure.com:3306/Pelican",
+                    "pelifar", "1234Fuckmekanikeren");
+            String sql = "DELETE FROM user WHERE userID=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1,deleteID);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public User findUserByID(int userID) {
+        for (User user : users) {
+            if (user.getUserID() == userID) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User findUserByUserName(String userName) {
+        for (User user:users) {
+            if (user.getUserName().equals(userName)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+
+    public void addRelation(User user){
+        int userID;
+        userID = 12345;
+        int userID2;
+        userID2 = 32414;
+
+        try {
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://pelican.mysql.database.azure.com:3306/Pelican",
+                    "pelifar", "1234Fuckmekanikeren");
+            String sql = "INSERT INTO relationtable VALUES(?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, userID);
+            preparedStatement.setInt(2, userID2);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public void addUserToShare(int activeUserID, int shareWithUserID) {
+        User shareWithUser = findUserByID(shareWithUserID);
+        ArrayList<Integer> newShareList = shareWithUser.getSharedWishlists();
+        newShareList.add(activeUserID);
+        shareWithUser.setSharedWishlists(newShareList);
+    }
+
+
+    public void deleteRelation(int deleteID){
+        try{
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://pelican.mysql.database.azure.com:3306/Pelican",
+                    "pelifar", "1234Fuckmekanikeren");
+            String sql = "DELETE FROM relationtable WHERE userID=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setInt(1,deleteID);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
