0eed39a34ecdb7119748f0d13a6551db936da8dd
benjovitz <dbenjovitz@gmail.com>
2022-10-27 13:44:55 +0200

    good shit

--- src/main/java/com/example/pelican/controller/WishController.java
+++ src/main/java/com/example/pelican/controller/WishController.java
@@ -4,10 +4,7 @@ import com.example.pelican.model.User;
 import com.example.pelican.repository.WishRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class WishController {
@@ -41,5 +38,10 @@ public class WishController {
     public  String deleteUser(@PathVariable("userID") int deleteID) {
         wishRepository.deleteUserById(deleteID);
         return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam("userName")String username){
+        return "login";
     }
 }
--- src/main/java/com/example/pelican/repository/WishRepository.java
+++ src/main/java/com/example/pelican/repository/WishRepository.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import java.sql.*;
+
 @Repository
 public class WishRepository {
 
